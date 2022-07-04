7cc3ed191f004a247c6d25f0df8897f7a949ecdf
Danilo Mongelli <info@danilomongelli.it>
2022-07-04 12:57:47 +0200

    new way to update model

--- src/Tab.tsx
+++ src/Tab.tsx
@@ -1,11 +1,6 @@
 import React, { lazy, useCallback, useEffect, useState } from "react";
 import { styled } from "@storybook/theming";
 import { Title, Source, Link } from "@storybook/components";
-import createEngine, {
-  DefaultLinkModel,
-  DefaultNodeModel,
-  DiagramModel,
-} from "@projectstorm/react-diagrams";
 import {
   useGlobals,
   useAddonState,
@@ -15,6 +10,14 @@ import {
   useParameter,
 } from "@storybook/api";
 import { ADDON_ID, EVENTS, PARAM_KEY } from "./constants";
+import createEngine, {
+  DiagramEngine,
+  DefaultLinkModel,
+  LinkModel,
+  LinkModelGenerics,
+  DefaultNodeModel,
+  DiagramModel,
+} from "@projectstorm/react-diagrams";
 import { CanvasWidget } from "@projectstorm/react-canvas-core";
 
 interface TabProps {
@@ -28,9 +31,11 @@ interface BuildModelInterface {
   storyArgs: typeof useArgs["arguments"];
 }
 
+const defaultNodeColor = "#F24581";
+
 const Wrapper = styled.div({
   width: "100%",
-  height: " 100%",
+  height: "100%",
   background:
     "conic-gradient(from 90deg at 1px 1px,#0000 90deg,#474747 0) 0 0/50px 50px",
   backgroundColor: "#333333",
@@ -38,80 +43,79 @@ const Wrapper = styled.div({
 
 const StyledCanvasWidget = styled(CanvasWidget)({
   width: "100%",
-  height: " 100%",
+  height: "100%",
 });
 
-const defaultNodeColor = "#F24581";
+const updateModel = ({ erDiagram, nodes, storyArgs }: BuildModelInterface) => {
+  const StoryNode = new DefaultNodeModel({
+    name: erDiagram.name,
+    color: "#029bf4",
+  });
+  StoryNode.setPosition(100, 50);
+  const StoryNodePort = StoryNode.addInPort(JSON.stringify(storyArgs));
 
-const engine = createEngine();
-const model = new DiagramModel();
-engine.setModel(model);
+  let buildedNodes = nodes[erDiagram.name]
+    ? nodes[erDiagram.name].map((brand, index) => {
+        console.log(brand);
+        const brandNode = new DefaultNodeModel({
+          name: brand,
+          color: defaultNodeColor,
+        });
+        brandNode.setPosition(index + 2 * 260, index * 120 + index);
+        brandNode.updateDimensions({ width: 200, height: 200 });
+        return brandNode;
+      })
+    : [];
 
-const buildModel = async ({
-  erDiagram,
-  nodes,
-  storyArgs,
-}: BuildModelInterface): Promise<boolean> => {
-  try {
-    const StoryNode = new DefaultNodeModel({
-      name: erDiagram.name,
-      color: "#029bf4",
-    });
-    StoryNode.setPosition(100, 50);
-    const StoryNodePort = StoryNode.addInPort(JSON.stringify(storyArgs));
+  buildedNodes = [StoryNode, ...buildedNodes];
 
-    let buildedNodes = nodes[erDiagram.name]
-      ? nodes[erDiagram.name].map((brand, index) => {
-          console.log(brand);
-          const brandNode = new DefaultNodeModel({
-            name: brand,
-            color: defaultNodeColor,
-          });
-          brandNode.setPosition(index + 2 * 260, index * 120 + index);
+  const links = buildedNodes.map((item, index) => {
+    if (index > 0) return item.addInPort("brand").link(StoryNodePort);
+  });
 
-          return brandNode;
-        })
-      : [];
-
-    buildedNodes = [StoryNode, ...buildedNodes];
-
-    const links = buildedNodes.map((item, index) => {
-      if (index > 0) return item.addInPort("brand").link(StoryNodePort);
-    });
-
-    model.addAll(...buildedNodes, ...links);
-
-    return true;
-  } catch (error) {
-    console.error("buildModel error:", error);
-    return false;
-  }
+  const result: (DefaultNodeModel | LinkModel<LinkModelGenerics>)[] = [
+    ...buildedNodes,
+    ...links,
+  ];
+  return result;
 };
 
 export const Tab: React.FC<TabProps> = ({ active }) => {
+  const diagramEngine = createEngine();
+  const activeModel = new DiagramModel();
+
+  const [engine, setEngine] = useState<DiagramEngine>(diagramEngine);
+  const [paintCanvas, setPaintCanvas] = useState(false);
+
   const [args, updateArgs, resetArgs] = useArgs();
   const [{ erDiagram }, updateGlobals] = useGlobals();
   const paramData = useParameter<{ [key: string]: string[] }>(PARAM_KEY);
-  const [showCanvas, setShowCanvas] = useState(false);
+
+  useEffect(() => {
+    return () => {
+      setEngine(null);
+    };
+  }, []);
 
   useEffect(() => {
     if (paramData && erDiagram) {
-      try {
-        buildModel({ erDiagram, nodes: paramData, storyArgs: args });
-        setShowCanvas(true);
-      } catch (error) {
-        console.error("buildModelError", error);
-      }
+      setPaintCanvas(false);
+      activeModel.addAll(
+        ...updateModel({ erDiagram, nodes: paramData, storyArgs: args })
+      );
+      engine.setModel(activeModel);
+      setEngine(engine);
+      setPaintCanvas(true);
     }
   }, [erDiagram, paramData]);
 
   return (
     <>
-      {active ? (
-        <Wrapper>
-          <StyledCanvasWidget engine={engine} />
-        </Wrapper>
-      ) : null}
+      <Wrapper>
+        {paintCanvas && (
+          <StyledCanvasWidget className="diagram-canvas" engine={engine} />
+        )}
+      </Wrapper>
     </>
   );
 };
